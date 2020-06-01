8c7c0206deecf721253069a047278cfb596f525a
solaris7x <solaris@belazy.ml>
2020-06-01 21:26:00 +0530

    CORS: Allow all headers

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "*"
+    // "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header("Accept-Ranges", "bytes");
   if (req.method === "OPTIONS") {
